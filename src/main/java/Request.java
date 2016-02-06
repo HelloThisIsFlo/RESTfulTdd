ac56b85b2492e23fc7ebe49887de8b05a10d6649
Florian Kempenich <ShockN745@gmail.com>
2016-02-06 19:01:28 +0100

    Extract service

---
+++ src/main/java/Request.java
@@ -0,0 +1,19 @@
+public class Request {
+
+    private String url;
+    private String service;
+
+    public Request(String url) {
+        this.url = url;
+        extractService();
+    }
+
+    private void extractService(){
+        String[] fragments = url.split("/");
+        service = fragments[1];
+    }
+
+    public String getService() {
+        return service;
+    }
+}
---
+++ src/test/java/RequestTest.java
@@ -0,0 +1,20 @@
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class RequestTest {
+
+    @Test
+    public void extractService() throws Exception {
+        String correctUrl = "/transactionservice/somethingelse";
+        Request request = new Request(correctUrl);
+        String result = request.getService();
+        assertEquals("transactionservice", result);
+
+        String wrongUrl = "/otherservice/test";
+        Request wrongRequest = new Request(wrongUrl);
+        String wrongResult = request.getService();
+
+        assertNotEquals("transactionservice", wrongRequest);
+    }
+}
